0ec8e38d431addc711c691e94280181f87e39093
IvanaTepavac <icca72@gmail.com>
2021-12-13 13:59:46 +0100

    Initial commit

---
+++ plural_kurs.py
@@ -0,0 +1,8 @@
+
+
+age = input('How old are you?\n')
+
+decades = int(age) // 10
+years = int(age) % 10
+
+print('You are ' + str(decades) + ' decades and ' + str(years) + ' years old.')
---
+++ recnik.py
@@ -0,0 +1,5 @@
+person = {'name': 'Ivana',
+          'age': 100,
+          'city': 'Bg'}
+# print(person.get('name'), 'is', person.get('age'), 'years old.')
+print(person['name'], 'is', person['age'], 'years old.')
